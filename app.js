333267291f02b66940ac8b58de0864d4c95fe005
MAY <cm0850068@otc.edu>
2018-09-10 09:54:20 -0500

    Maximum done

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ let ofMusketeers;
 for (ofMusketeers of musketeers){
     console.log(ofMusketeers);
 }
-*/
+
 
 const values =[3,11,7,2,9,10];
 let sum=0;
@@ -26,4 +26,13 @@ for (i=0; i<values.length; i++){
     sum += values[i];
 }
 console.log(sum);
+*/
+const values =[3,11,7,2,9,10];
+let max=0
+for (i=0; i<values.length; i++){
+    if (values[i]>max){
+        max=values[i];
+    }
+}
+console.log(max);
 
